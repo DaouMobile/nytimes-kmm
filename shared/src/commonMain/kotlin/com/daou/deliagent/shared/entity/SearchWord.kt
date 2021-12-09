4887b9b3d8bad5daef5af99b001d8350a590796c
SangEun <seun0101@daou.co.kr>
2021-12-09 17:32:41 +0900

    update searchword entitiy

--- shared/src/commonMain/kotlin/com/daou/deliagent/shared/entity/SearchWord.kt
+++ shared/src/commonMain/kotlin/com/daou/deliagent/shared/entity/SearchWord.kt
@@ -1,3 +1,12 @@
 package com.daou.deliagent.shared.entity
 
-data class SearchWord()
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SearchWord(
+    @SerialName("id")
+    var id: Long,
+    @SerialName("keyword")
+    var keyword: String
+)
