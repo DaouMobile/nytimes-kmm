ab908c42e4d2097948f8bebcac4bb6ffdd2233eb
Geonu Jeon <geonu1109@gmail.com>
2022-01-04 17:39:33 +0900

    feat: Add iOS project

---
+++ iosApp/NYTimes/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  NYTimes
+//
+//  Created by Geonu Jeon on 2022/01/04.
+//
+
+import SwiftUI
+import shared
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
